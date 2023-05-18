00d7a29114e27b3f632a309fd64555b087184bb1
ReemAlmamdouh <reem.almamdouh@gmail.com>
2023-05-18 06:18:04 -0700

    end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,12 +9,12 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-dlistint_t *new_node;
-new_node = malloc(sizeof(dlistint_t));
+dlistint_t *new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 {
 return (NULL);
 }
+
 new_node->n = n;
 new_node->next = NULL;
 
@@ -26,12 +26,13 @@ new_node->prev = NULL;
 else
 {
 dlistint_t *last_node = *head;
-while (last_node != NULL)
+while (last_node->next != NULL)
 {
 last_node = last_node->next;
 }
 last_node->next = new_node;
 new_node->prev = last_node;
 }
+
 return (new_node);
 }
