8e91700bb099e9b8eddee43778c44f9ff848d4fd
ReemAlmamdouh <reem.almamdouh@gmail.com>
2023-05-18 05:42:13 -0700

    head

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -6,10 +6,10 @@
  * n: integer
  * Return: address of the new element or NULL if failed
  **/
-
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new_node = malloc(sizeof(dlistint_t));
+dlistint_t *new_node;
+new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 {
 return (NULL);
