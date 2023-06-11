62d58f32d244eb3b9e3a3e2bcc327bad7d1eb3c7
ReemAlmamdouh <reem.almamdouh@gmail.com>
2023-06-11 04:37:19 -0700

    update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,11 +9,12 @@
 int main(void)
 {
 	int sum, num;
+
 	for (num > 0; num < 1024; num++)
 	{
 		if ((num % 3 == 0) || (num % 5 == 0))
 			sum += num;
 	}
 	printf("%d\n", sum);
-	reruen (0);
+	reruen(0);
 }
